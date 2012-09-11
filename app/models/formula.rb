de57a4cc9bf4e571ac1ac8a4cc9b2e3da42e2c4b
Damián Farina <damianfarina@gmail.com>
2012-09-11 11:13:42 -0300

    Solucionado el tema de la validación del nombre de la fórmula

--- app/models/formula.rb
+++ app/models/formula.rb
@@ -8,20 +8,28 @@ class Formula < ActiveRecord::Base
     :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
 
   validates :abrasive, :grain, :hardness, :porosity, :alloy, :formula_items, :name, :presence => true
-  validates :name, :uniqueness => true
+  validate :name_is_unique, :unless => 'self.name.nil?'
   validate :items_proportion_is_one_hundred, :if => 'self.formula_items.any?'
 
   before_validation :set_name
 
-  private
+private
 
   def items_proportion_is_one_hundred
     difference = 100.0
     difference = self.formula_items.inject(difference) { |result, item| result -= (item.proportion || 0)  }
-    errors[:base] << I18n.t(:items_proportion_should_be_100, :scope => [:activerecord, :errors, :models, :formula], :difference => difference.round(3)) unless (difference.abs < 0.01)
+    errors[:base] << I18n.t(:items_proportion_should_be_100, :scope => [:activerecord, :errors, :models, :formula],
+      :difference => difference.round(3)) unless (difference.abs < 0.01)
   end
 
   def set_name
     self.name = (abrasive + grain + hardness + porosity + alloy).gsub(' ', '')
+  end
+
+  def name_is_unique
+    other_formula = Formula.find_by_name(self.name)
+    errors[:base] << I18n.t(:name_must_be_unique, :scope => [:activerecord, :errors, :models, :formula],
+      :other_formula_id => other_formula.id,
+      :other_formula_name => other_formula.name) if other_formula and other_formula.id != self.id
   end
 end
