a4b2b472dff12818791ce567498187c2c84a9e9e
Damián Farina <damianfarina@gmail.com>
2012-11-11 18:12:25 -0300

    La migra que actualiza las cantidades de stock de materia prima consumidas tras crear una orden de fabricación

---
+++ db/migrate/20121111205911_add_consumed_stock_to_making_order_formula_items.rb
@@ -0,0 +1,12 @@
+class AddConsumedStockToMakingOrderFormulaItems < ActiveRecord::Migration
+  def up
+    add_column :making_order_formula_items, :consumed_stock, :decimal
+    MakingOrderFormulaItem.find_each(:batch_size => 10) do |m|
+      m.update_attributes :consumed_stock => m.making_order_formula.making_order.total_weight * m.proportion / 100.0
+    end
+  end
+
+  def down
+    remove_column :making_order_formula_items, :consumed_stock
+  end
+end
