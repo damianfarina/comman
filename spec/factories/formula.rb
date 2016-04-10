b6ba0f92f4e407fc72ffac7555f44ca6b95f73c6
Damian Farina <damianfarina@gmail.com>
2016-04-10 02:53:34 -0300

    Products resource, still need to make those tests pass

---
+++ spec/factories/formula.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :formula do
+    name 'Test Formula'
+    abrasive '49A'
+    grain '20.3'
+    hardness 'R'
+    porosity '6'
+    alloy 'BAK'
+  end
+end
---
+++ spec/factories/products.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :product do
+    formula_id 1
+    shape 'CPL'
+    size '127X50X22.2'
+    weight 1.45
+    pressure 'C-20'
+    price 100.0
+  end
+end
