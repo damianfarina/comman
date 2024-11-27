b662f7c0fb1c10433e879cd1646f3cde23577708
Damián Farina <damianfarina@gmail.com>
2024-11-27 16:50:29 -0300

    Add pagination gem

--- app/controllers/factory/formula_elements_controller.rb
+++ app/controllers/factory/formula_elements_controller.rb
@@ -4,7 +4,7 @@ module Factory
 
     # GET /formula_elements or /formula_elements.json
     def index
-      @formula_elements = FormulaElement.all
+      @formula_elements = FormulaElement.page(params[:page]).per(10)
     end
 
     # GET /formula_elements/1 or /formula_elements/1.json
