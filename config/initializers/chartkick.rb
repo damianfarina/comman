be09d2efbb3795da6229bed1eb0fd600ca5378df
Damián Farina <damianfarina@gmail.com>
2024-11-28 13:34:44 -0300

    Add chartkick

---
+++ config/initializers/chartkick.rb
@@ -0,0 +1,17 @@
+Chartkick.options = {
+  height: "20rem",
+  colors: [
+    "#60D394",
+    "#EE6054",
+    "#FFC233",
+    "#235789",
+    "#57534e",
+    "#FAFAFA",
+    "#d6d3d1",
+    "#9FE5BE",
+    "#FF9B85",
+    "#FFE199",
+    "#C9DDFF",
+    "#1c1917"
+  ],
+}
