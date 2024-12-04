4e4561198016096977ce83151c87ed4c31171fb4
Damián Farina <damianfarina@gmail.com>
2024-12-04 01:14:39 -0300

    Disable https for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,13 +25,13 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
-  config.assume_ssl = true
+  config.assume_ssl = false
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = false
 
   # Skip http-to-https redirect for the default health check endpoint.
-  config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
+  # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
 
   # Log to STDOUT with the current request id as a default log tag.
   config.log_tags = [ :request_id ]
@@ -80,13 +80,13 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
-  # Enable DNS rebinding protection and other `Host` header attacks.
-  config.hosts = [
-    IPAddr.new("0.0.0.0/0") # Allow requests from any IP address
+    # Enable DNS rebinding protection and other `Host` header attacks.
+    # config.hosts = [
+    # IPAddr.new("0.0.0.0/0") # Allow requests from any IP address
     # "example.com",     # Allow requests from example.com
     # /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  ]
+  # ]
   #
   # Skip DNS rebinding protection for the default health check endpoint.
-  config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
--- db/production_setup.sql
+++ db/production_setup.sql
@@ -1,28 +1,6 @@
 -- db/production_setup.sql
 
-DO $$
-BEGIN
-  IF NOT EXISTS (
-    SELECT FROM pg_database WHERE datname = 'comman_production'
-  ) THEN
-    PERFORM dblink_exec('postgres', 'CREATE DATABASE comman_production');
-  END IF;
-
-  IF NOT EXISTS (
-    SELECT FROM pg_database WHERE datname = 'comman_production_cache'
-  ) THEN
-    PERFORM dblink_exec('postgres', 'CREATE DATABASE comman_production_cache');
-  END IF;
-
-  IF NOT EXISTS (
-    SELECT FROM pg_database WHERE datname = 'comman_production_queue'
-  ) THEN
-    PERFORM dblink_exec('postgres', 'CREATE DATABASE comman_production_queue');
-  END IF;
-
-  IF NOT EXISTS (
-    SELECT FROM pg_database WHERE datname = 'comman_production_cable'
-  ) THEN
-    PERFORM dblink_exec('postgres', 'CREATE DATABASE comman_production_cable');
-  END IF;
-END $$;
+CREATE DATABASE comman_production;
+CREATE DATABASE comman_production_cache;
+CREATE DATABASE comman_production_queue;
+CREATE DATABASE comman_production_cable;
