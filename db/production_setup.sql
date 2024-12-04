4804f79019124f0407d9dca8f4dc4a107cd4dcfe
Damián Farina <damianfarina@gmail.com>
2024-12-03 23:55:11 -0300

    Make sure that if one db already exists the others are created

--- db/production_setup.sql
+++ db/production_setup.sql
@@ -1,6 +1,28 @@
 -- db/production_setup.sql
 
-CREATE DATABASE comman_production;
-CREATE DATABASE comman_production_cache;
-CREATE DATABASE comman_production_queue;
-CREATE DATABASE comman_production_cable;
+DO $$
+BEGIN
+  IF NOT EXISTS (
+    SELECT FROM pg_database WHERE datname = 'comman_production'
+  ) THEN
+    PERFORM dblink_exec('postgres', 'CREATE DATABASE comman_production');
+  END IF;
+
+  IF NOT EXISTS (
+    SELECT FROM pg_database WHERE datname = 'comman_production_cache'
+  ) THEN
+    PERFORM dblink_exec('postgres', 'CREATE DATABASE comman_production_cache');
+  END IF;
+
+  IF NOT EXISTS (
+    SELECT FROM pg_database WHERE datname = 'comman_production_queue'
+  ) THEN
+    PERFORM dblink_exec('postgres', 'CREATE DATABASE comman_production_queue');
+  END IF;
+
+  IF NOT EXISTS (
+    SELECT FROM pg_database WHERE datname = 'comman_production_cable'
+  ) THEN
+    PERFORM dblink_exec('postgres', 'CREATE DATABASE comman_production_cable');
+  END IF;
+END $$;
