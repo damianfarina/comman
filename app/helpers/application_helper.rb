647465beeaf83274958b12ba2b5e7166376c8201
Damián Farina <damianfarina@gmail.com>
2024-12-10 20:56:51 -0300

    Fix navigation links for nested resources

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def sidebar_link(name, path, icon)
     default_classes = "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-lightBlue hover:bg-blue-700 hover:text-white"
     active_classes = "bg-blue-700 text-white"
-    classes = current_page?(path) ? "#{default_classes} #{active_classes}" : default_classes
+    classes = current_or_nested_page?(path) ? "#{default_classes} #{active_classes}" : default_classes
     aria_current = current_page?(path) ? "page" : false
 
     content_tag(:li) do
@@ -13,12 +13,16 @@ module ApplicationHelper
     end
   end
 
+  def current_or_nested_page?(path)
+    current_page?(path) || request.path.start_with?(path)
+  end
+
   def navigation_items
     [
       { name: t("navigation.dashboard"), path: factory_dashboard_index_path, icon: "home" },
       { name: t("titles.making_order.index"), path: factory_making_orders_path, icon: "circle-stack" },
       { name: t("titles.formula.index"), path: factory_formulas_path, icon: "beaker" },
-      { name: t("titles.formula_element.index"), path: factory_formula_elements_path, icon: "puzzle-piece" }
+      { name: t("titles.formula_element.index"), path: factory_formula_elements_path, icon: "puzzle-piece" },
     ]
   end
 end
