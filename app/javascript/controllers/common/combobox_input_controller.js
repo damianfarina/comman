d7315cf4b6b12d8f8db57bc052bf1c058e9ca333
Damián Farina <damianfarina@gmail.com>
2024-12-14 18:18:40 -0300

    WIP combobox input

---
+++ app/javascript/controllers/common/combobox_input_controller.js
@@ -0,0 +1,76 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="common--combobox-input"
+export default class extends Controller {
+  static targets = ["button", "menu", "option", "input"];
+  static classes = ["highlight", "hidden"];
+
+  connect() {
+    this.open = false;
+    this.selectedOption = null;
+    this.filterOptions();
+  }
+
+  toggle() {
+    this.open = !this.open;
+    this.updateMenu();
+  }
+
+  close() {
+    this.open = false;
+    this.updateMenu();
+  }
+
+  select(event) {
+    this.selectedOption = event.currentTarget;
+    this.close();
+    this.updateButton();
+  }
+
+  highlight(event) {
+    this.optionTargets.forEach((option) =>
+      option.classList.remove(...this.highlightClasses)
+    );
+    event.currentTarget.classList.add(...this.highlightClasses);
+  }
+
+  filter(event) {
+    const query = event.target.value.toLowerCase();
+    this.optionTargets.forEach((option) => {
+      const text = option.textContent.toLowerCase();
+      option.classList.toggle(...this.hiddenClasses, !text.includes(query));
+    });
+    this.open = true;
+    this.updateMenu();
+  }
+
+  filterOptions() {
+    const query = this.inputTarget.value.toLowerCase()
+    this.optionTargets.forEach(option => {
+      const text = option.textContent.toLowerCase()
+      option.classList.toggle(...this.hiddenClasses, !text.includes(query))
+    })
+  }
+
+  focus() {
+    this.open = true;
+    this.updateMenu();
+  }
+
+  updateMenu() {
+    if (this.open) {
+      this.menuTarget.classList.remove(...this.hiddenClasses);
+      this.buttonTarget.setAttribute("aria-expanded", "true");
+    } else {
+      this.menuTarget.classList.add(...this.hiddenClasses);
+      this.buttonTarget.setAttribute("aria-expanded", "false");
+    }
+  }
+
+  updateButton() {
+    if (this.selectedOption) {
+      this.buttonTarget.querySelector("span").textContent =
+        this.selectedOption.querySelector("span").textContent;
+    }
+  }
+}
