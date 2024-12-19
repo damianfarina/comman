9a451c9d558c49a71fd96966f86c026aa92fd2be
Damián Farina <damianfarina@gmail.com>
2024-12-19 16:15:38 -0300

    Fix fields_for child_index and show errors

--- app/models/formula.rb
+++ app/models/formula.rb
@@ -8,7 +8,8 @@ class Formula < ApplicationRecord
 
   validates :abrasive, :grain, :hardness, :porosity, :alloy, :name, :formula_items, presence: true
   validate :name_is_unique, if: -> { name.present? }
-  validate :items_proportion_is_one_hundred, if:  -> { self.formula_items.any? }
+  validate :no_duplicated_formula_elements
+  validate :items_proportion_is_one_hundred, if: -> { self.formula_items.any? }
 
   before_validation :set_name
 
@@ -30,22 +31,43 @@ class Formula < ApplicationRecord
         result
       end
 
-      return if difference.abs < 0.01
+      if difference.abs >= 0.01
+        formula_items.each do |item|
+          item.errors.add(:base,
+            I18n.t(:proportion_must_be_one_hundred, scope: [ :activerecord, :errors, :models, :formula ])
+          )
+        end
 
-      errors.add(
-        :base,
-        I18n.t(
-          :proportion_must_be_one_hundred,
-          scope: [ :activerecord, :errors, :models, :formula ],
-          difference: difference.round(3),
+        errors.add(
+          :base,
+          I18n.t(
+            :proportion_must_be_one_hundred,
+            scope: [ :activerecord, :errors, :models, :formula ],
+            difference: difference.round(3),
+          )
         )
-      )
 
-      throw :abort
+        throw :abort
+      end
     end
 
     def set_name
       self.name = "#{abrasive}#{grain}#{hardness}#{porosity}#{alloy}".gsub(" ", "")
+    end
+
+    def no_duplicated_formula_elements
+      element_ids = formula_items.reject(&:marked_for_destruction?).map(&:formula_element_id)
+      duplicates = element_ids.select { |id| element_ids.count(id) > 1 }.uniq
+
+      if duplicates.any?
+        formula_items.each do |item|
+          if duplicates.include?(item.formula_element_id) && !item.marked_for_destruction?
+          item.errors.add(:base, I18n.t(:duplicated_formula_elements, scope: [ :activerecord, :errors, :models, :formula ]))
+          end
+        end
+        errors.add(:base, I18n.t(:duplicated_formula_elements, scope: [ :activerecord, :errors, :models, :formula ]))
+        throw :abort
+      end
     end
 
     def name_is_unique
