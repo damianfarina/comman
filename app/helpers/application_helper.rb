04b15f94cf06a732893e4fb8fbb3ae3201f8e40f
Damián Farina <damianfarina@gmail.com>
2025-02-03 21:16:53 -0300

    Upgrade tailwindcss to v4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
   def sidebar_link(name, path, icon)
-    default_classes = "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-lightBlue hover:bg-blue-700 hover:text-white"
+    default_classes = "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-light-blue hover:bg-blue-700 hover:text-white"
     active_classes = "bg-blue-700 text-white"
     classes = current_or_nested_page?(path) ? "#{default_classes} #{active_classes}" : default_classes
     aria_current = current_page?(path) ? "page" : false
 
     content_tag(:li) do
       content_tag(:a, href: path, class: classes, 'aria-current': aria_current) do
-        concat heroicon(icon, options: { class: "h-6 w-6 shrink-0 text-lightBlue group-hover:text-white" })
+        concat heroicon(icon, options: { class: "h-6 w-6 shrink-0 text-light-blue group-hover:text-white" })
         concat name
       end
     end
