5d003de6819bc4a755d41908741865ccda249e97
Damián Farina <damianfarina@gmail.com>
2025-04-01 18:06:38 -0300

    Set pretty email from

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV.fetch("SENDGRID_FROM")
+  default from: email_address_with_name(
+    ENV.fetch("SENDGRID_FROM_EMAIL", "SENDGRID_FROM_EMAIL"),
+    ENV.fetch("SENDGRID_FROM_NAME", "SENDGRID_FROM_NAME")
+  )
   layout "mailer"
 end
