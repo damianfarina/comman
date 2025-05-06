d1c4fa2f3d71ac0e6e1cfdef5d7ff0a33ab19a07
Damián Farina <damianfarina@gmail.com>
2025-05-06 00:56:18 -0300

    Enable recent activity on office dashboard

--- app/controllers/office/dashboard_controller.rb
+++ app/controllers/office/dashboard_controller.rb
@@ -1,6 +1,9 @@
 module Office
   class DashboardController < ApplicationController
     def index
+      @recent_activities = PaperTrail::Version.order(created_at: :desc).limit(10)
+      user_ids = @recent_activities.pluck(:whodunnit).compact.uniq
+      @version_user_names = User.where(id: user_ids).pluck(:id, :name).to_h
     end
   end
 end
