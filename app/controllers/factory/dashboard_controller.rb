e961bd05c6c8e9dadd605c2e061d292f61f2c5de
Damián Farina <damianfarina@gmail.com>
2025-05-17 19:10:16 -0300

    Show product activity feed

--- app/controllers/factory/dashboard_controller.rb
+++ app/controllers/factory/dashboard_controller.rb
@@ -1,6 +1,10 @@
 module Factory
   class DashboardController < ApplicationController
     def index
+      @audit_logs = AuditLog
+        .where(auditable_type: "Product", auditable_id: Product.manufactured_products.select(:id))
+        .includes(:auditable, :user)
+        .recent
     end
   end
 end
--- app/controllers/office/dashboard_controller.rb
+++ app/controllers/office/dashboard_controller.rb
@@ -1,6 +1,7 @@
 module Office
   class DashboardController < ApplicationController
     def index
+      @audit_logs = AuditLog.recent.includes(:auditable, :user)
     end
   end
 end
