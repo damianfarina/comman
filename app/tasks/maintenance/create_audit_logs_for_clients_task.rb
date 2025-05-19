263c4d058f6e26f1ff4b426dd4ffa94a791de407
Damián Farina <damianfarina@gmail.com>
2025-05-19 14:44:31 -0300

    Create audit log records for existing clients

---
+++ app/tasks/maintenance/create_audit_logs_for_clients_task.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Maintenance
+  class CreateAuditLogsForClientsTask < MaintenanceTasks::Task
+    def collection
+      Client.left_outer_joins(:audit_logs).where(audit_logs: { id: nil })
+        .or(Client.left_outer_joins(:audit_logs).where.not(audit_logs: { action: "create" }))
+        .distinct
+    end
+
+    def process(client)
+      return if client.audit_logs.exists?(action: "create")
+
+      AuditLog.create!(
+        action: "create",
+        auditable: client,
+        audited_changes: { "_created" => [ nil, client.name ] },
+        audited_fields: [ "_created" ],
+        created_at: client.created_at,
+        transaction_id: "created by maintenance task: #{self.class.name}",
+        user_id: nil
+      )
+    end
+
+    def count
+      self.collection.count
+    end
+  end
+end
