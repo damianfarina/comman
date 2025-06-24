009c185d7b36402e600d66ff8ca717951355ba76
Damián Farina <damianfarina@gmail.com>
2025-06-24 12:01:24 -0300

    Add sales_order_items presence validation

--- spec/factories/sales_order_items.rb
+++ spec/factories/sales_order_items.rb
@@ -1,9 +1,13 @@
 FactoryBot.define do
   factory :sales_order_item do
-    sales_order { association :sales_order }
-    product { association :product }
+    sales_order
+    product
     quantity { 2 }
     unit_price { nil }
     status { :quote }
+
+    trait :with_product do
+      product { association :purchased_productable }
+    end
   end
 end
