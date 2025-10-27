bf516e2652fa1751bcd344531b8188933e9711c1
Damián Farina <damianfarina@gmail.com>
2025-10-27 18:05:02 -0300

    Proxy assets instead of redirect

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,9 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  # Resolve Active Storage routes through the application
+  config.active_storage.resolve_model_to_route = :rails_storage_proxy
+
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   config.assume_ssl = true
 
